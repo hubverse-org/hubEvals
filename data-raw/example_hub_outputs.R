802d4f19749b57649f81e49b24ab59b445c928bd
Evan Ray <elray@umass.edu>
2024-01-17 19:04:24 -0500

    update example quantile model output data

--- data-raw/example_hub_outputs.R
+++ data-raw/example_hub_outputs.R
@@ -3,7 +3,16 @@
 library(hubUtils)
 
 hub_path <- "data-raw/example-simple-forecast-hub"
-example_model_outputs <- hubUtils::connect_hub(hub_path) %>%
+example_model_outputs <- hubUtils::connect_hub(hub_path) |>
   dplyr::collect()
 
-usethis::use_data(example_model_outputs, overwrite = TRUE)
+q_lvls_keep <- c("0.050", "0.100", "0.250", "0.500", "0.750", "0.900", "0.950")
+example_quantile_model_output <- example_model_outputs |>
+  dplyr::filter(
+    horizon %in% c(0, 7),
+    location %in% c("25", "US"),
+    output_type == "quantile",
+    format(output_type_id, nsmall = 3) %in% q_lvls_keep
+  )
+
+usethis::use_data(example_quantile_model_output, overwrite = TRUE)
