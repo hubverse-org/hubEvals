a176fdad0b9512fba2d378a470eaaa735ca8b987
Anna Krystalli <annakrystalli@googlemail.com>
2024-02-13 10:35:06 +0200

    Fix linting issues

--- data-raw/example_target_data.R
+++ data-raw/example_target_data.R
@@ -5,8 +5,10 @@ library(hubUtils)
 load("data/example_quantile_model_output.rda")
 
 hub_path <- "data-raw/example-simple-forecast-hub"
-target_data_path <- file.path(hub_path, "target-data",
-                              "covid-hospitalizations.csv")
+target_data_path <- file.path(
+  hub_path, "target-data",
+  "covid-hospitalizations.csv"
+)
 
 raw_target_data <- read.csv(target_data_path)
 raw_target_data <- raw_target_data |>
@@ -17,9 +19,12 @@ example_quantile_model_output <- example_quantile_model_output |>
 
 example_target_data <- example_quantile_model_output |>
   dplyr::distinct(
-    origin_date, horizon, location, target, target_date) |>
+    origin_date, horizon, location, target, target_date
+  ) |>
   dplyr::left_join(
-    raw_target_data, by = c("location", "target_date", "target")) |>
+    raw_target_data,
+    by = c("location", "target_date", "target")
+  ) |>
   dplyr::select(-target_date)
 
 head(example_target_data)
