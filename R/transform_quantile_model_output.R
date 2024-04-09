fb609d763aa49ba3a2a4a43259e5c1de444ccf26
kimberlynroosa <kroosa@utk.edu>
2024-04-09 13:19:18 -0400

    adding transform_quantile_model_output function, addressing issue #22

--- R/transform_quantile_model_output.R
+++ R/transform_quantile_model_output.R
@@ -1,29 +1,34 @@
-#' Title
+#' Transform quantile model output into a forecast object
 #'
-#' @param proj_data
-#' @param obs_data
+#' @param proj_data Model output projection data frame
+#' @param obs_data Observed 'ground truth' data to be compared against forecasts
 #'
-#' @return
-#' @export
+#' @return forecast_quantile
+#'
+#' @importFrom dplyr %>%
 #'
 #' @examples
 
 transform_quantile_model_output <- function(proj_data, obs_data) {
 
-  data <- left_join(proj_data, obs_data, by = c("target","location","target_end_date","output_type"))
+  proj_data <- proj_data %>%
+    dplyr::filter(output_type == "quantile") %>%
+    dplyr::mutate(output_type_id = as.numeric(output_type_id))
 
-  forecast <- data %>%
-    filter(data, output_type == "quantile") %>%
-    scoringutils::as_forecast(
-      forecast_unit = c("model","location","target_end_date","horizon"),
-      forecast_type = "quantile",
-      observed = "observed",
-      predicted = "predicted",
-      model = "model",
-      quantile_level = "quantile_level"
-    )
+  obs_data <- obs_data %>%
+    dplyr::filter(output_type == "quantile") %>%
+    select(-output_type_id)
 
+  data <- dplyr::left_join(proj_data, obs_data,
+                    by = c("target","location","target_end_date","output_type"))
+
+  forecast_quantile <- scoringutils::as_forecast(data,
+                                        forecast_unit = c("model","location","reference_date","target_end_date","horizon"),
+                                        forecast_type = "quantile",
+                                        observed = "observation",
+                                        predicted = "value",
+                                        model = "model_id",
+                                        quantile_level = "output_type_id")
 
   return(forecast_quantile)
-
 }
