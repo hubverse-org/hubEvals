6e09adc24d9bcbd97cf3c4c91747e132ff2e9fba
Evan Ray <elray@umass.edu>
2024-08-26 10:35:24 -0400

    fix up comment spacing

--- tests/testthat/test-score_model_out.R
+++ tests/testthat/test-score_model_out.R
@@ -81,7 +81,7 @@ test_that("score_model_out succeeds with valid inputs: mean output_type, default
 
 
 test_that("score_model_out succeeds with valid inputs: mean output_type, character metrics, custom by", {
-   # Forecast data from HubExamples: <https://hubverse-org.github.io/hubExamples/reference/forecast_data.html>
+  # Forecast data from HubExamples: <https://hubverse-org.github.io/hubExamples/reference/forecast_data.html>
   load(test_path("testdata/forecast_outputs.rda")) # sets forecast_outputs
   load(test_path("testdata/forecast_target_observations.rda")) # sets forecast_target_observations
 
