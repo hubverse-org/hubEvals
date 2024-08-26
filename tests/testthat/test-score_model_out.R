4c6eabca84823db65f1077287ccadc64254c32e4
Evan Ray <elray1@users.noreply.github.com>
2024-08-26 10:13:53 -0400

    Update tests/testthat/test-score_model_out.R
    Co-authored-by: Zhian N. Kamvar <zkamvar@gmail.com>

--- tests/testthat/test-score_model_out.R
+++ tests/testthat/test-score_model_out.R
@@ -484,6 +484,7 @@ test_that("score_model_out errors when invalid metrics are requested", {
 
 
 test_that("score_model_out errors when an unsupported output_type is provided", {
+  # Forecast data from HubExamples: <https://hubverse-org.github.io/hubExamples/reference/forecast_data.html>
   load(test_path("testdata/forecast_outputs.rda")) # sets forecast_outputs
   load(test_path("testdata/forecast_target_observations.rda")) # sets forecast_target_observations
 
