23675188f62b9d1540f2be233b5a64d61b1e7ba3
Evan Ray <elray1@users.noreply.github.com>
2024-08-26 10:14:59 -0400

    Update tests/testthat/test-score_model_out.R
    Co-authored-by: Zhian N. Kamvar <zkamvar@gmail.com>

--- tests/testthat/test-score_model_out.R
+++ tests/testthat/test-score_model_out.R
@@ -297,6 +297,7 @@ test_that("score_model_out succeeds with valid inputs: quantile output_type, wis
 
 
 test_that("score_model_out succeeds with valid inputs: quantile output_type, wis/interval metrics, summarize FALSE", {
+  # Forecast data from HubExamples: <https://hubverse-org.github.io/hubExamples/reference/forecast_data.html>
   load(test_path("testdata/forecast_outputs.rda")) # sets forecast_outputs
   load(test_path("testdata/forecast_target_observations.rda")) # sets forecast_target_observations
 
